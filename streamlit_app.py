c9f763b9df537409082ffb7b4a14c2c0cebe3695
Syed Muhammad Junaid Abbas <syedhoney0@gmail.com>
2026-01-08 00:26:33 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         
         # Step 5: Use the 'search_on' variable in the API call
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + {search_on})
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Insert logic remains the same
